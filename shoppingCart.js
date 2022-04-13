e5acdd93dab46ac1907a4db4ee5d1a422666df41
Claire Baissac <claire.baissac@yoco.com>
2022-04-13 11:31:00 +0200

    shopping cart functions

--- shoppingCart.js
+++ shoppingCart.js
@@ -10,10 +10,26 @@ function addToCart(product) {
   }
 }
 
-function removeFromCart(product) {}
+function removeFromCart(product) {
+  let cart = localStorage.getItem("cart").split(",");
+  const index = cart.indexOf(product.id);
+  if (index > -1) {
+    cart.splice(index, 1);
+  }
+  localStorage.setItem("cart", cart.toString());
+}
 
-function getCartTotal() {}
+function getCartTotal() {
+  const cart = localStorage.getItem("cart");
+  if (cart) {
+    return cart.split(",").length;
+  } else {
+    return 0;
+  }
+}
 
-function clearCart() {}
+function clearCart() {
+  localStorage.removeItem("cart");
+}
 
 export { addToCart, removeFromCart, getCartTotal, clearCart };
