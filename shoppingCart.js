5ce8aff6cb213950ba9a10af2f7cf4cc9cc4ee59
Claire Baissac <claire.baissac@yoco.com>
2022-04-13 13:58:31 +0200

    fix localStorage to store serialised JSON

--- shoppingCart.js
+++ shoppingCart.js
@@ -1,31 +1,31 @@
 function addToCart(product) {
   try {
-    let cart = localStorage.getItem("cart").split(",");
-    cart.push(product.id);
-    localStorage.setItem("cart", cart.toString());
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    cart.push(product);
+    localStorage.setItem("cart", JSON.stringify(cart));
   } catch (error) {
-    let cart = [product.id];
+    let cart = [product];
     console.log(cart);
-    localStorage.setItem("cart", cart.toString());
+    localStorage.setItem("cart", JSON.stringify(cart));
   }
 }
 
 function removeFromCart(product) {
-  let cart = localStorage.getItem("cart").split(",");
-  const index = cart.indexOf(product.id);
-  if (index > -1) {
-    cart.splice(index, 1);
-  }
-  localStorage.setItem("cart", cart.toString());
+  // let cart = localStorage.getItem("cart").split(",");
+  // const index = cart.indexOf(product.id);
+  // if (index > -1) {
+  //   cart.splice(index, 1);
+  // }
+  // localStorage.setItem("cart", cart.toString());
 }
 
 function getCartTotal() {
-  const cart = localStorage.getItem("cart");
-  if (cart) {
-    return cart.split(",").length;
-  } else {
-    return 0;
-  }
+  // const cart = localStorage.getItem("cart");
+  // if (cart) {
+  //   return cart.split(",").length;
+  // } else {
+  //   return 0;
+  // }
 }
 
 function clearCart() {
